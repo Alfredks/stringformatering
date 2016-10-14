b2d92b684873dcdbb95f116a1d7e4c32b75466bb
alfredks <alfredks@WS-VGY-1941>
2016-10-14 13:45:39 +0200

    Stringform

--- stringformatering/Program.cs
+++ stringformatering/Program.cs
@@ -10,6 +10,30 @@ namespace stringformatering
     {
         static void Main(string[] args)
         {
+            //övning1 
+            /*  DateTime datum = DateTime.Now;
+
+              Console.WriteLine(datum.ToString("d"));
+              Console.ReadLine();
+
+            //övning 2
+            double pi = Math.PI;
+            double area = pi * 4 * 4;
+
+            Console.WriteLine(area.ToString("F4"));
+            Console.ReadLine();
+
+            //övning 3
+            int x = 1000000000;
+            Console.WriteLine("{0:0,0,0}", x);
+
+            //övning 4 
+            double x = 123.00;
+            Console.WriteLine("det blir {0} kr och {1:00} öre", x,00); */
+
+            //övning 5
+
+
         }
     }
 }
